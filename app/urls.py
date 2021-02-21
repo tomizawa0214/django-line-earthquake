f5cd551569b6aefed1beb62303d35e1e3be4bed3
tomizawa0214 <j.tomizawa.japan@gmail.com>
2021-02-21 16:11:36 +0900

    オウム返しテスト

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from app import views
+from .views import CallbackView
+
+app_name = 'app'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('callback/', CallbackView.as_view(), name='callback_view'),
 ]
